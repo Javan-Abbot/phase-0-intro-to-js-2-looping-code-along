312c93c0df77b8ba4290000866efa6084cd86dac
+javan abbot <+>
2024-07-10 13:03:13 +0300

    looping done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arrayName,event){
+    const newArray=[];
+for(let i=0;i<arrayName.length;i++){
+    console.log(i);
+
+  newArray.push(`Thank you, ${arrayName[i]}, for the wonderful ${event} gift!`)
+}
+
+return newArray;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num){
+    for(let i=num;i>=0;i--){
+        console.log(i);
+    }
+}
